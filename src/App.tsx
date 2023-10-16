d7394c715ed092af663961ff1711aea519a48472
Xu <deep.work.max@gmail.com>
2023-10-16 10:33:53 +0800

    disable middleware

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 import SpeedTestIp from './getip/speedtest'
 import IpCn from "./getip/ipcn";
 import AmazonIP from "./getip/amazonip";
 
 function App() {
-  return (
-    <div className="App">
-        <SpeedTestIp/>
-        <IpCn/>
-        <AmazonIP/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <SpeedTestIp/>
+            <IpCn/>
+            <AmazonIP/>
+        </div>
+    );
 }
 
 export default App;
